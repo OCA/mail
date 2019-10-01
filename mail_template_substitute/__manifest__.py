74a4a34fe3f1fd10dc3e873c724e559ba6de0dba
sbejaoui <souheil.bejaoui@acsone.eu>
2019-10-01 16:30:44 +0200

    [12.0][ADD] - Add unit tests

--- mail_template_substitute/__manifest__.py
+++ mail_template_substitute/__manifest__.py
@@ -6,11 +6,11 @@
     "summary": """
         This module allows to create substitution rules for mail templates.
         """,
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,"
               "Odoo Community Association (OCA)",
-    "website": "https://github.com/acsone/social",
+    "website": "https://github.com/OCA/social",
     "depends": ["base", "mail", "report_substitute"],
     "data": [
         "security/mail_template_substitution_rule.xml",
