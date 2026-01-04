6d9c3de72acc19e4b29a151fb025936b40601b02
TheSumitBasak <sumitbasak2208@gmail.com>
2026-01-04 12:05:27 +0000

    [IMP] mail_activity_unlink_log: pre-commit auto fixes

--- mail_activity_unlink_log/__manifest__.py
+++ mail_activity_unlink_log/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/social",
+    "website": "https://github.com/OCA/mail",
     "depends": ["mail"],
     "data": ["data/unlink_message.xml"],
 }
