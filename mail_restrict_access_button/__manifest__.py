f57c83eaf7e7156497e44600045d2e1b4466a2cc
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2026-06-09 10:26:19 +0200

    [ADD] mail_restrict_access_button

---
+++ mail_restrict_access_button/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Restrict Access Button",
+    "version": "18.0.1.0.0",
+    "category": "Discuss",
+    "license": "AGPL-3",
+    "summary": "Show the notification access button only to recipients with "
+    "a user account (internal or portal), never to unregistered partners.",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/mail",
+    "depends": ["mail"],
+    "installable": True,
+}
