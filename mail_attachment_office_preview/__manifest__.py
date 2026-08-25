9054a00606e930983dd090edbaae869462e85ae1
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2026-08-25 15:00:22 -0600

    [ADD] mail_attachment_office_preview: preview Office attachments via LibreOffice

---
+++ mail_attachment_office_preview/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2026 Jarsa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Attachment Office Preview",
+    "summary": "Preview Office attachments in the browser "
+    "by converting them to PDF with LibreOffice",
+    "version": "17.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/mail",
+    "author": "Jarsa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "assets": {
+        "web.assets_backend": [
+            "mail_attachment_office_preview/static/src/attachment_model_patch.esm.js",
+            "mail_attachment_office_preview/static/src/file_viewer_patch.esm.js",
+            "mail_attachment_office_preview/static/src/file_viewer_patch.xml",
+        ],
+    },
+    "installable": True,
+}
